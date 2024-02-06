d51342638ab45df0bd92fb0a1f3be02d4a520ac1
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-02-06 17:28:25 +0100

    Add script to sync folders

---
+++ update.sh
@@ -0,0 +1,24 @@
+# Copyright (C) 2024 Jacopo Donati
+# 
+# This file is part of dotfiles.
+# 
+# dotfiles is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# dotfiles is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with dotfiles.  If not, see <http://www.gnu.org/licenses/>.
+
+# This is a simple script that copies my dotfiles from $HOME to the repo
+
+#!/bin/zsh
+
+rsync -aE --delete ~/.emacs.d ./
+rsync -aE --delete ~/.vim ./
+rsync -aE --delete ~/.yt-dlp ./
